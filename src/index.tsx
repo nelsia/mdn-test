5c80f007db2804c526e70f90970b1177de092d4a
nelsia <dev.nelsia@gmail.com>
2020-11-30 10:56:01 +0900

    Add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
+import styled from "styled-components";
+
+const Header = styled.h1`
+  color: red;
+  `
 
 const Main = (
-  <div>
-    <h1>Markdown Editor</h1>
-    <hr />
-    <div>TEST</div>
-  </div>
+    <Header>Markdown Editor</Header>
 );
 
 render(Main, document.getElementById("app"));
