0418f5f65e16305601017f62a7f539dd5c6c4038
nelsia <dev.nelsia@gmail.com>
2020-11-30 15:51:41 +0900

    implement react-router

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -6,6 +6,8 @@ import { putMemo } from "../indexeddb/memos";
 import { Button } from "../components/button";
 import { SaveModal } from "../components/save_modal";
 
+import { Link } from "react-router-dom";
+
 const { useState } = React
 
 const Header = styled.header`
@@ -78,6 +80,9 @@ export const Editor: React.FC = () => {
           <Button onClick={() => setShowModal(true)}>
             保存する
           </Button>
+          <Link to="/history">
+            履歴を見る
+          </Link>
         </HeaderControl>
       </Header>
       <Wrapper>
