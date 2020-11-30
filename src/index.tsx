6cbf275a6e129d0de20fc7c36cbfe212d6951589
nelsia <dev.nelsia@gmail.com>
2020-11-30 10:51:46 +0900

    Add webpack-dev-server

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import * as React from "react";
 import { render } from "react-dom";
 
 const Main = (
-  <h1>Markdown Editor</h1>
+  <div>
+    <h1>Markdown Editor</h1>
+    <hr />
+    <div>TEST</div>
+  </div>
 );
 
 render(Main, document.getElementById("app"));
